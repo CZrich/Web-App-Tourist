561426c87a09858f87109ed6bc6652f569e4dffa
CZrich <czrichardz@gmail.com>
2024-07-14 23:10:58 -0500

    se implemento get y post para servicio

--- webAppIPS/src/Services/servicios.turistico.js
+++ webAppIPS/src/Services/servicios.turistico.js
@@ -3,15 +3,32 @@ const URL_SERVICIOS="http://100.29.145.50/api/v1/services";
 const URL_EMPLEADOS ="https://jsonplaceholder.typicode.com/users";
 
 
-const URL_PRUEBA_SERVICIO="/servicios.json";
+const URL_PRUEBA_SERVICIO="http://127.0.0.1:2024/api/servicios";
 //const sevircios=axios.create({URL_SERVICIOS});
   
-export function GetServiciosTuristico() {
+export function getServiciosTuristico() {
   return axios.get(URL_PRUEBA_SERVICIO);
 
 
 }
-export function GetEmpleados(){
-  return axios.get(URL_EMPLEADOS);
+export function postServicioTuristico(servicio){
 
+  return axios.post(URL_PRUEBA_SERVICIO,servicio);
+}
+
+export function CrearProducto(producto){
+  return producApi.post('/',producto);
+
+}
+export function getProducto(id){
+  return producApi.get('/'+id+'/');
+}
+export function EliminarProducto(id){
+return producApi.delete('/'+id);
+}
+export function ActualizarProducto(id,producto){
+  return producApi.put('/'+id+'/',producto);
+}
+export function GetEmpleados( ){
+  return axios.get(URL_EMPLEADOS);
 }
